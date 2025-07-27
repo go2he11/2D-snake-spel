8db409b8bedc559bb431cac92795f49f30706ddc
Ahmad Shukor <shukor.ah@gmail.com>
2025-07-27 21:21:53 +0200

    kod på rörelse, mat placering, ormens tillväxt, kollision men vägg och sig själv, omstart

---
+++ Assets/Food.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public BoxCollider2D gridArea;
+
+    private void Start()
+    {
+        RandomizePosition();
+    }
+
+private void RandomizePosition()
+{
+    Bounds bounds = gridArea.bounds;
+
+    float x = Mathf.Floor(Random.Range(bounds.min.x, bounds.max.x));
+    float y = Mathf.Floor(Random.Range(bounds.min.y, bounds.max.y));
+
+    this.transform.position = new Vector3(x, y, 0.0f);
+}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            RandomizePosition();
+        }
+    }
+}
